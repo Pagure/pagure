066e85e0de173b7f25128c9a7f8300fd136c629e
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-18 15:27:48 +0100

    Add the createdb script to initialize the DB

---
+++ createdb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
+from progit import APP
+from progit import model
+
+model.create_tables(
+    APP.config['DB_URL'],
+    APP.config.get('PATH_ALEMBIC_INI', None),
+    True)
