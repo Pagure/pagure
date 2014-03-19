ab2f8f3b141ecde4bcf16058de890bf1eb2c6156
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-19 09:58:15 +0100

    Add the form to create or edit a project

---
+++ progit/forms.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+
+"""
+ (c) 2014 - Copyright Red Hat Inc
+
+ Authors:
+   Pierre-Yves Chibon <pingou@pingoured.fr>
+
+"""
+
+from flask.ext import wtf
+import wtforms
+
+
+class ProjectForm(wtf.Form):
+    ''' Form to create or edit project. '''
+    name = wtforms.TextField(
+        'Project name <span class="error">*</span>',
+        [wtforms.validators.Required()]
+    )
+    description = wtforms.TextField(
+        'description',
+        [wtforms.validators.optional()]
+    )
