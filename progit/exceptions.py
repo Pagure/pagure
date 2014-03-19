6420e6ff944ec9a0d96e4eba0447c0ddf8742200
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-19 09:57:59 +0100

    Add the first exceptions

---
+++ progit/exceptions.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+
+"""
+ (c) 2014 - Copyright Red Hat Inc
+
+ Authors:
+   Pierre-Yves Chibon <pingou@pingoured.fr>
+
+"""
+
+
+class ProgitException(Exception):
+    ''' Parent class of all the exception for all Progit specific
+    exceptions.
+    '''
+    pass
+
+
+class RepoExistsException(ProgitException):
+    ''' Exception thrown when trying to create a repository that already
+    exists.
+    '''
+    pass
