556a8ed933f3ce574f2a1550b7a2f20bd61b97f5
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-26 10:10:18 +0100

    Simplify the issue related url
    Turns out we can use the same function for both projects and forks urls
    
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- progit/issues.py
+++ progit/issues.py
@@ -26,7 +26,85 @@ import progit.forms
 from progit import APP, SESSION, LOG, __get_file_in_tree, cla_required
 
 
-def do_new_issue(repo, username=None):
+## URLs
+
+@APP.route('/<repo>/issue/<issueid>/add',
+           methods=('GET','POST'))
+@APP.route('/fork/<username>/<repo>/issue/<issueid>/add',
+           methods=('GET', 'POST'))
+def add_comment_issue(repo, issueid, username=None):
+    ''' Add a comment to an issue. '''
+    repo = progit.lib.get_project(SESSION, repo, user=username)
+
+    if repo is None:
+        flask.abort(404, 'Project not found')
+
+    if not repo.issue_tracker:
+        flask.abort(404, 'No issue tracker found for this project')
+
+    issue = progit.lib.get_issue(SESSION, issueid)
+
+    if issue is None:
+        flask.abort(404, 'Issue not found')
+
+    form = progit.forms.AddIssueCommentForm()
+    if form.validate_on_submit():
+        comment = form.comment.data
+
+        try:
+            message = progit.lib.add_issue_comment(
+                SESSION,
+                issue=issue,
+                comment=comment,
+                user=flask.g.fas_user.username,
+            )
+            SESSION.commit()
+            flask.flash(message)
+        except SQLAlchemyError, err:  # pragma: no cover
+            SESSION.rollback()
+            flask.flash(str(err), 'error')
+
+    return flask.redirect(flask.url_for(
+        'view_issue', username=username, repo=repo.name, issueid=issue.id))
+
+
+@APP.route('/<repo>/issues')
+@APP.route('/fork/<username>/<repo>/issues')
+def view_issues(repo, username=None):
+    """ List all issues associated to a repo
+    """
+    status = flask.request.args.get('status', None)
+
+    repo = progit.lib.get_project(SESSION, repo, user=username)
+
+    if repo is None:
+        flask.abort(404, 'Project not found')
+
+    if not repo.issue_tracker:
+        flask.abort(404, 'No issue tracker found for this project')
+
+    if status is not None:
+        if status.lower() == 'closed':
+            issues = progit.lib.get_issues(SESSION, repo, closed=True)
+        else:
+            issues = progit.lib.get_issues(SESSION, repo, status=status)
+    else:
+        issues = progit.lib.get_issues(SESSION, repo, status='Open')
+
+    return flask.render_template(
+        'issues.html',
+        select='issues',
+        repo=repo,
+        username=username,
+        status=status,
+        issues=issues,
+    )
+
+
+@APP.route('/<repo>/new_issue', methods=('GET', 'POST'))
+@APP.route('/fork/<username>/<repo>/new_issue', methods=('GET', 'POST'))
+@cla_required
+def new_issue(repo, username=None):
     """ Create a new issue
     """
     repo = progit.lib.get_project(SESSION, repo, user=username)
@@ -66,68 +144,14 @@ def do_new_issue(repo, username=None):
     )
 
 
-def do_edit_issue(repo, issueid, username=None):
-    """ Edit the specified issue
-    """
-    repo = progit.lib.get_project(SESSION, repo, user=username)
-
-    if repo is None:
-        flask.abort(404, 'Project not found')
-
-    if not repo.issue_tracker:
-        flask.abort(404, 'No issue tracker found for this project')
-
-    issue = progit.lib.get_issue(SESSION, issueid)
-
-    if issue is None:
-        flask.abort(404, 'Issue not found')
-
-    status = progit.lib.get_issue_statuses(SESSION)
-    form = progit.forms.IssueForm(status=status)
-    if form.validate_on_submit():
-        title = form.title.data
-        content = form.content.data
-        status = form.status.data
-
-        try:
-            message = progit.lib.edit_issue(
-                SESSION,
-                issue=issue,
-                title=title,
-                content=content,
-                status=status,
-            )
-            SESSION.commit()
-            flask.flash(message)
-            url = flask.url_for('view_issues', repo=repo.name)
-            if username:
-                url = flask.url_for(
-                    'view_fork_issues', username=username, repo=repo.name)
-            return flask.redirect(url)
-        except progit.exceptions.ProgitException, err:
-            flask.flash(str(err), 'error')
-        except SQLAlchemyError, err:  # pragma: no cover
-            SESSION.rollback()
-            flask.flash(str(err), 'error')
-    elif flask.request.method == 'GET':
-        form.title.data = issue.title
-        form.content.data = issue.content
-        form.status.data = issue.status
-
-    return flask.render_template(
-        'new_issue.html',
-        select='issues',
-        type='edit',
-        form=form,
-        repo=repo,
-        username=username,
-        issue=issue,
-    )
-
-
-def do_view_issue(repo, issueid, username=None):
+@APP.route('/<repo>/issue/<issueid>', methods=('GET', 'POST'))
+@APP.route('/fork/<username>/<repo>/issue/<issueid>',
+           methods=('GET', 'POST'))
+@cla_required
+def view_issue(repo, issueid, username=None):
     """ List all issues associated to a repo
     """
+
     repo = progit.lib.get_project(SESSION, repo, user=username)
 
     if repo is None:
@@ -174,43 +198,13 @@ def do_view_issue(repo, issueid, username=None):
     )
 
 
-def do_view_issues(repo, username=None, status=None):
-    """ List all issues associated to a repo
-    """
-    repo = progit.lib.get_project(SESSION, repo, user=username)
-
-    if repo is None:
-        flask.abort(404, 'Project not found')
-
-    if not repo.issue_tracker:
-        flask.abort(404, 'No issue tracker found for this project')
-
-    if status is not None:
-        if status.lower() == 'closed':
-            issues = progit.lib.get_issues(SESSION, repo, closed=True)
-        else:
-            issues = progit.lib.get_issues(SESSION, repo, status=status)
-    else:
-        issues = progit.lib.get_issues(SESSION, repo, status='Open')
-
-    return flask.render_template(
-        'issues.html',
-        select='issues',
-        repo=repo,
-        username=username,
-        status=status,
-        issues=issues,
-    )
-
-
-## URLs
-
-@APP.route('/<repo>/issue/<issueid>/add',
-           methods=('GET','POST'))
-@APP.route('/fork/<username>/<repo>/issue/<issueid>/add',
+@APP.route('/<repo>/issue/<issueid>/edit', methods=('GET', 'POST'))
+@APP.route('/fork/<username>/<repo>/issue/<issueid>/edit',
            methods=('GET', 'POST'))
-def add_comment_issue(repo, issueid, username=None):
-    ''' Add a comment to an issue. '''
+@cla_required
+def edit_issue(repo, issueid, username=None):
+    """ Edit the specified issue
+    """
     repo = progit.lib.get_project(SESSION, repo, user=username)
 
     if repo is None:
@@ -224,89 +218,43 @@ def add_comment_issue(repo, issueid, username=None):
     if issue is None:
         flask.abort(404, 'Issue not found')
 
-    form = progit.forms.AddIssueCommentForm()
+    status = progit.lib.get_issue_statuses(SESSION)
+    form = progit.forms.IssueForm(status=status)
     if form.validate_on_submit():
-        comment = form.comment.data
+        title = form.title.data
+        content = form.content.data
+        status = form.status.data
 
         try:
-            message = progit.lib.add_issue_comment(
+            message = progit.lib.edit_issue(
                 SESSION,
                 issue=issue,
-                comment=comment,
-                user=flask.g.fas_user.username,
+                title=title,
+                content=content,
+                status=status,
             )
             SESSION.commit()
             flask.flash(message)
+            url = flask.url_for(
+                'view_issue', username=username,
+                repo=repo.name, issueid=issue.id)
+            return flask.redirect(url)
+        except progit.exceptions.ProgitException, err:
+            flask.flash(str(err), 'error')
         except SQLAlchemyError, err:  # pragma: no cover
             SESSION.rollback()
             flask.flash(str(err), 'error')
+    elif flask.request.method == 'GET':
+        form.title.data = issue.title
+        form.content.data = issue.content
+        form.status.data = issue.status
 
-    return flask.redirect(flask.url_for(
-        'view_issue', username=username, repo=repo.name, issueid=issue.id))
-
-
-@APP.route('/<repo>/issues')
-def view_issues(repo):
-    """ List all issues associated to a repo
-    """
-    status = flask.request.args.get('status', None)
-    return do_view_issues(repo, status=status)
-
-
-@APP.route('/fork/<username>/<repo>/issues')
-def view_fork_issues(repo, username):
-    """ List all issues associated to a repo
-    """
-    status = flask.request.args.get('status', None)
-    return do_view_issues(repo, username=username, status=status)
-
-
-@APP.route('/<repo>/new_issue', methods=('GET', 'POST'))
-@cla_required
-def new_issue(repo):
-    """ Create a new issue
-    """
-    return do_new_issue(repo)
-
-
-@APP.route('/fork/<username>/<repo>/new_issue', methods=('GET', 'POST'))
-@cla_required
-def fork_new_issue(username, repo):
-    """ Create a new issue
-    """
-    return do_new_issue(repo, username=username)
-
-
-@APP.route('/<repo>/issue/<issueid>', methods=('GET', 'POST'))
-@cla_required
-def view_issue(repo, issueid):
-    """ List all issues associated to a repo
-    """
-    return do_view_issue(repo, issueid)
-
-
-@APP.route('/fork/<username>/<repo>/issue/<issueid>',
-           methods=('GET', 'POST'))
-@cla_required
-def view_fork_issue(username, repo, issueid):
-    """ List all issues associated to a repo
-    """
-    return do_view_issue(repo, issueid, username=username)
-
-
-@APP.route('/<repo>/issue/<issueid>/edit', methods=('GET', 'POST'))
-@cla_required
-def edit_issue(repo, issueid):
-    """ Edit the specified issue
-    """
-    return do_edit_issue(repo, issueid)
-
-
-@APP.route('/fork/<username>/<repo>/issue/<issueid>/edit',
-           methods=('GET', 'POST'))
-@cla_required
-def fork_edit_issue(username, repo, issueid):
-    """ Edit the specified issue opened against a fork
-    """
-    return do_edit_issue(repo, issueid, username=username)
-
+    return flask.render_template(
+        'new_issue.html',
+        select='issues',
+        type='edit',
+        form=form,
+        repo=repo,
+        username=username,
+        issue=issue,
+    )
