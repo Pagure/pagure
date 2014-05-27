3976d7bec4dfdb331f1e275e650c1bc85e62bc92
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-05-27 11:21:00 +0200

    Add a global set_up method for the hooks
    This method install the post-receive shell script allowing us to have
    multiple post-receive hooks per project.

--- progit/hooks/__init__.py
+++ progit/hooks/__init__.py
@@ -8,12 +8,36 @@
 
 """
 
+import os
+
+from progit import APP
+
 
 class BaseHook(object):
     ''' Base class for progit's hooks. '''
 
     name = None
     form = None
+
+    @classmethod
+    def set_up(cls, project):
+        ''' Install the generic post-receive hook that allow us to call
+        multiple post-receive hooks as set per plugin.
+        '''
+        repopath = os.path.join(APP.config['GIT_FOLDER'], project.path)
+        if project.is_fork:
+            repopath = os.path.join(APP.config['FORK_FOLDER'], project.path)
+
+        hook_files = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), 'files')
+
+        # Install the main post-receive file
+        postreceive = os.path.join(repopath, 'hooks', 'post-receive')
+        if not os.path.exists(postreceive):
+            shutil.copyfile(
+                os.path.join(hook_files, 'post-receive'),
+                postreceive)
+            os.chmod(postreceive, 0755)
 
     @classmethod
     def install(cls, project):
