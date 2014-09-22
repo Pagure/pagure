362d6b3efb8772a5d8beceb692146754f6afe892
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-09-22 13:34:14 +0200

    We can compare objects are they are both DB mapping object
    Reverts 2ec4241

--- progit/issues.py
+++ progit/issues.py
@@ -164,7 +164,7 @@ def view_issue(repo, issueid, username=None):
 
     issue = progit.lib.get_issue(SESSION, repo.id, issueid)
 
-    if issue is None or issue.project.name != repo.name:
+    if issue is None or issue.project != repo:
         flask.abort(404, 'Issue not found')
 
     status = progit.lib.get_issue_statuses(SESSION)
