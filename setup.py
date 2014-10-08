88ffcab41ca41b09b39dd13a404b976a19328e72
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-08 10:04:46 +0200

    Add a setup.py file to build releases

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""
+Setup script
+"""
+
+# Required to build on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
+from setuptools import setup
+from progit import __version__
+
+
+def get_requirements(requirements_file='requirements.txt'):
+    """Get the contents of a file listing the requirements.
+
+    :arg requirements_file: path to a requirements file
+    :type requirements_file: string
+    :returns: the list of requirements, or an empty list if
+              `requirements_file` could not be opened or read
+    :return type: list
+    """
+
+    lines = open(requirements_file).readlines()
+    return [
+        line.rstrip().split('#')[0]
+        for line in lines
+        if not line.startswith('#')
+    ]
+
+
+setup(
+    name='progit',
+    description='A light-weight git-centered forge based on pygit2..',
+    version=__version__,
+    author='Pierre-Yves Chibon',
+    author_email='pingou@pingoured.fr',
+    maintainer='Pierre-Yves Chibon',
+    maintainer_email='pingou@pingoured.fr',
+    license='GPLv2+',
+    download_url='https://fedorahosted.org/releases/p/r/progit/',
+    url='https://fedorahosted.org/progit/',
+    packages=['progit'],
+    include_package_data=True,
+    install_requires=get_requirements(),
+)
