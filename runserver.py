281f754ff98b0b362903778d5e606952a2f7ca3f
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-16 09:49:59 +0200

    Add the option to get profiling information via runserver

--- runserver.py
+++ runserver.py
@@ -4,6 +4,14 @@
 __requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
 import pkg_resources
 
+import sys
+from werkzeug.contrib.profiler import ProfilerMiddleware
+
 from progit import APP
 APP.debug = True
+
+if '--profile' in sys.argv:
+    APP.config['PROFILE'] = True
+    APP.wsgi_app = ProfilerMiddleware(APP.wsgi_app, restrictions=[30])
+
 APP.run()
