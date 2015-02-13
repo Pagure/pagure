db25ca3ed37befa677a0879f02314834967b61e8
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-02-13 17:32:00 +0100

    Let the default configuration be less secure but working out of the box
    This is really interesting to allow to work on progit without having to
    write your own configuration file

--- progit/default_config.py
+++ progit/default_config.py
@@ -123,7 +123,7 @@ CHECK_SESSION_IP = True
 SHORT_LENGTH = 6
 
 # Make browsers send session cookie only via HTTPS
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 
 # Used by SESSION_COOKIE_PATH
-APPLICATION_ROOT = '/progit'
+APPLICATION_ROOT = '/'
