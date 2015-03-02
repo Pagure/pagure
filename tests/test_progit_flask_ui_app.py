e5e1071448cd48bf1197a7708b4007d3aa14be44
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-03-02 17:04:12 +0100

    Add unit-tests for the new_project endpoint

--- tests/test_progit_flask_ui_app.py
+++ tests/test_progit_flask_ui_app.py
@@ -108,6 +108,74 @@ class ProgitFlaskApptests(tests.Modeltests):
             '<section class="project_list" id="forks">' in output.data)
         self.assertTrue('<h2>Forks (0)</h2>' in output.data)
 
+    def test_new_project(self):
+        """ Test the new_project endpoint. """
+        # Before
+        projects = progit.lib.search_projects(self.session)
+        self.assertEqual(len(projects), 0)
+        self.assertFalse(os.path.exists(
+            os.path.join(tests.HERE, 'project#1.git')))
+        self.assertFalse(os.path.exists(
+            os.path.join(tests.HERE, 'tickets', 'project#1.git')))
+        self.assertFalse(os.path.exists(
+            os.path.join(tests.HERE, 'docs', 'project#1.git')))
+
+        user = tests.FakeUser()
+        with tests.user_set(progit.APP, user):
+            output = self.app.get('/new/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<h2>New project</h2>' in output.data)
+
+            csrf_token = output.data.split(
+                'name="csrf_token" type="hidden" value="')[1].split('">')[0]
+
+            data = {
+                'description': 'Project #1',
+            }
+
+            output = self.app.post('/new/', data=data)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<h2>New project</h2>' in output.data)
+            self.assertTrue(
+                '<td class="errors">This field is required.</td>'
+                in output.data)
+
+            data['name'] = 'project#1'
+            output = self.app.post('/new/', data=data)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<h2>New project</h2>' in output.data)
+            self.assertFalse(
+                '<td class="errors">This field is required.</td>'
+                in output.data)
+
+            data['csrf_token'] =  csrf_token
+            output = self.app.post('/new/', data=data)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<h2>New project</h2>' in output.data)
+            self.assertTrue(
+                '<li class="error">No user &#34;username&#34; found</li>'
+                in output.data)
+
+        user.username = 'foo'
+        with tests.user_set(progit.APP, user):
+            data['csrf_token'] =  csrf_token
+            output = self.app.post('/new/', data=data, follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<p>Project #1</p>' in output.data)
+            self.assertTrue(
+                '<li class="message">Project &#34;project#1&#34; created</li>'
+                in output.data)
+
+        # After
+        projects = progit.lib.search_projects(self.session)
+        self.assertEqual(len(projects), 1)
+        self.assertTrue(os.path.exists(
+            os.path.join(tests.HERE, 'project#1.git')))
+        self.assertTrue(os.path.exists(
+            os.path.join(tests.HERE, 'tickets', 'project#1.git')))
+        self.assertTrue(os.path.exists(
+            os.path.join(tests.HERE, 'docs', 'project#1.git')))
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(ProgitFlaskApptests)
