3839a6d03c51b674e5f305bc448e0e4f5d148ed5
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-03-13 19:10:51 +0100

    Fix the createdb script to import the model properly

--- createdb.py
+++ createdb.py
@@ -5,7 +5,7 @@ __requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
 import pkg_resources
 
 from progit import APP
-from progit import model
+from progit.lib import model
 
 model.create_tables(
     APP.config['DB_URL'],
