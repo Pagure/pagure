e77021141dc0ca8d77dc1624481092d22f969219
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-03-25 14:48:07 +0100

    Add a sample fedmsg configuration file

---
+++ fedmsg.d/pagure.py
@@ -0,0 +1,20 @@
+""" This is an example pagure configuration for fedmsg.
+By convention, it is normally installed as ``/etc/fedmsg.d/pagure.py``
+
+For Fedora Infrastructure, our own version of this file is kept in
+``ansible/roles/fedmsg/base/templates/``
+
+It needs to be globally available so remote consumers know how to find the
+pagure producer (wsgi process).
+"""
+
+import socket
+hostname = socket.gethostname().split('.')[0]
+
+config = dict(
+    endpoints={
+        "pagure.%s" % hostname: [
+            "tcp://127.0.0.1:3005",
+        ],
+    },
+)
