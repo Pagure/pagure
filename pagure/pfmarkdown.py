7cea35a842a4937fe64690a31b7d3488ed9e6f8b
Ralph Bean <rbean@redhat.com>
2015-03-26 10:24:34 -0400

    Add more and improve existing types of issue matching.

--- pagure/pfmarkdown.py
+++ pagure/pfmarkdown.py
@@ -40,11 +40,11 @@ def inject():
         def handleMatch(self, m):
             el = markdown.util.etree.Element("a")
             name = markdown.util.AtomicString(m.group(2))
-            el.set('href', _user_url(name[1:]))
-            el.text = name
+            el.set('href', _user_url(name))
+            el.text = ' @%s' % name
             return el
 
-    class ExplicitIssuePattern(markdown.inlinepatterns.Pattern):
+    class ExplicitForkIssuePattern(markdown.inlinepatterns.Pattern):
         def handleMatch(self, m):
             el = markdown.util.etree.Element("a")
             user = markdown.util.AtomicString(m.group(2))
@@ -54,18 +54,48 @@ def inject():
             el.text = '%s/%s#%s' % (user, repo, idx)
             return el
 
-    MENTION_RE = r'(@\w+)'
-    EXPLICIT_ISSUE_RE = r'(\w+)/(\w+)#([0-9]+)'
+    class ExplicitMainIssuePattern(markdown.inlinepatterns.Pattern):
+        def handleMatch(self, m):
+            el = markdown.util.etree.Element("a")
+            repo = markdown.util.AtomicString(m.group(2))
+            idx = markdown.util.AtomicString(m.group(3))
+            el.set('href', _issue_url(None, repo, idx))
+            el.text = ' %s#%s' % (repo, idx)
+            return el
+
+    class ImplicitIssuePattern(markdown.inlinepatterns.Pattern):
+        def handleMatch(self, m):
+            el = markdown.util.etree.Element("a")
+            idx = markdown.util.AtomicString(m.group(2))
+
+            root = flask.request.url_root
+            url = flask.request.url
+            user = None
+            if 'fork/' in flask.request.url:
+                user, repo = url.split('fork/')[1].split('/', 2)[:2]
+            else:
+                repo = url.split(root)[1].split('/', 1)[0]
+
+            el.set('href', _issue_url(user, repo, idx))
+            el.text = ' #%s' % idx
+            return el
+
+    MENTION_RE = r'[^|\w]@(\w+)'
+    EXPLICIT_FORK_ISSUE_RE = r'(\w+)/(\w+)#([0-9]+)'
+    EXPLICIT_MAIN_ISSUE_RE = r'[^|\w](?<!\/)(\w+)#([0-9]+)'
+    IMPLICIT_ISSUE_RE = r'[^|\w](?<!\w)#([0-9]+)'
 
     # Lastly, monkey-patch the build_inlinepatterns func to insert our patterns
     original_builder = markdown.build_inlinepatterns
 
-    def extended_builder(md_instance, **kwargs):
-        patterns = original_builder(md_instance, **kwargs)
-        patterns['mention'] = MentionPattern(
-            MENTION_RE, md_instance)
-        patterns['explicit_issue'] = ExplicitIssuePattern(
-            EXPLICIT_ISSUE_RE, md_instance)
+    def extended_builder(m, **kwargs):
+        patterns = original_builder(m, **kwargs)
+        patterns['mention'] = MentionPattern(MENTION_RE, m)
+        patterns['explicit_fork_issue'] = ExplicitForkIssuePattern(
+            EXPLICIT_FORK_ISSUE_RE, m)
+        patterns['explicit_main_issue'] = ExplicitMainIssuePattern(
+            EXPLICIT_MAIN_ISSUE_RE, m)
+        patterns['implicit_issue'] = ImplicitIssuePattern(IMPLICIT_ISSUE_RE, m)
         return patterns
 
     markdown.build_inlinepatterns = extended_builder
