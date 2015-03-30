1ac7fd4d783698c69243509ce451f105a73759ea
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-03-30 14:31:47 +0200

    Have pagure respect the configuration when sending emails

--- pagure/lib/notify.py
+++ pagure/lib/notify.py
@@ -98,8 +98,7 @@ def send_email(text, subject, to_mail, from_mail=None, mail_id=None,
 
     msg = MIMEText(text.encode('utf-8'), 'plain', 'utf-8')
     msg['Subject'] = '[Pagure] %s' % subject
-    if not from_mail:
-        from_email = 'pagure@fedoraproject.org'
+    from_email = pagure.APP.config('FROM_EMAIL', 'pagure@fedoraproject.org')
     msg['From'] = from_email
     msg['Bcc'] = to_mail.replace(',', ', ')
 
