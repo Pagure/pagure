36a05f7d482cbdbfc317b81f1b0eba6116595d6a
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-04-14 21:19:15 +0200

    Clean the FROM field before considering it for the hash

--- milters/comment_email_milter.py
+++ milters/comment_email_milter.py
@@ -130,7 +130,7 @@ class PagureMilter(Milter.Base):
         # Ensure the user replied to his/her own notification, not that
         # they are trying to forge their ID into someone else's
         salt = pagure.APP.config.get('SALT_EMAIL')
-        m = hashlib.sha512('%s%s%s' % (msg_id, salt, msg['From']))
+        m = hashlib.sha512('%s%s%s' % (msg_id, salt, clean_item(msg['From'])))
         tohash= msg['to'].split('@')[0].split('+')[-1]
         if m.hexdigest() != tohash:
             self.log('hash: %s' % m.hexdigest())
