993597847fe614826031c5b158eb9949d5cd4811
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-05-11 18:56:11 +0200

    Add unit-tests checking the api_new_issue endpoint

---
+++ tests/test_progit_flask_api_issue.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+"""
+ (c) 2015 - Copyright Red Hat Inc
+
+ Authors:
+   Pierre-Yves Chibon <pingou@pingoured.fr>
+
+"""
+
+__requires__ = ['SQLAlchemy >= 0.8']
+import pkg_resources
+
+import unittest
+import shutil
+import sys
+import os
+
+import json
+from mock import patch
+
+sys.path.insert(0, os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '..'))
+
+import pagure.lib
+import tests
+
+
+class PagureFlaskApiIssuetests(tests.Modeltests):
+    """ Tests for the flask API of pagure for issue """
+
+    def setUp(self):
+        """ Set up the environnment, ran before every tests. """
+        super(PagureFlaskApiIssuetests, self).setUp()
+
+        pagure.APP.config['TESTING'] = True
+        pagure.SESSION = self.session
+        pagure.api.SESSION = self.session
+        pagure.api.issue.SESSION = self.session
+        pagure.lib.SESSION = self.session
+        self.app = pagure.APP.test_client()
+
+    def test_api_new_issue(self):
+        """ Test the token based authentication.
+        """
+        tests.create_projects(self.session)
+        tests.create_tokens(self.session)
+        tests.create_acls(self.session)
+        tests.create_tokens_acl(self.session)
+
+        headers = {'Authorization': 'token aaabbbcccddd'}
+
+        # Valid token, wrong project
+        output = self.app.post('/api/0/test2/new_issue', headers=headers)
+        self.assertEqual(output.status_code, 401)
+        data = json.loads(output.data)
+        self.assertDictEqual(
+            data,
+            {
+              "error": "Invalid or expired token. Please visit " \
+                  "https://pagure.org/ get or renew your API token.",
+              "error_code": 5
+            }
+        )
+
+        # No input
+        output = self.app.post('/api/0/test/new_issue', headers=headers)
+        self.assertEqual(output.status_code, 400)
+        data = json.loads(output.data)
+        self.assertDictEqual(
+            data,
+            {
+              "error": "Invalid or incomplete input submited",
+              "error_code": 4
+            }
+        )
+
+        data = {
+            'title': 'test issue',
+        }
+
+        # Incomplete request
+        output = self.app.post(
+            '/api/0/test/new_issue', data=data, headers=headers)
+        self.assertEqual(output.status_code, 400)
+        data = json.loads(output.data)
+        self.assertDictEqual(
+            data,
+            {
+              "error": "Invalid or incomplete input submited",
+              "error_code": 4
+            }
+        )
+
+        data = {
+            'title': 'test issue',
+            'issue_content': 'This issue needs attention',
+            'status': 'Open',
+        }
+
+        # Valid request
+        output = self.app.post(
+            '/api/0/test/new_issue', data=data, headers=headers)
+        self.assertEqual(output.status_code, 200)
+        data = json.loads(output.data)
+        self.assertDictEqual(
+            data,
+            {'message': 'issue created'}
+        )
+
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(
+        PagureFlaskApiIssuetests)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
