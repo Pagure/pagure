29ee20fd68932e5a3a59374378d85c4c6e1b0813
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-05-12 15:15:25 +0200

    More ACLs to the token used in the tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -259,8 +259,8 @@ def create_tokens(session, user_id=1):
 def create_acls(session):
     """ Create some acls for the tokens. """
     for acl in [
-            'create_issue', 'update_issue', 'pull_request_comment',
-            'change_status_issue', 'comment_issue'
+            'create_issue', 'pull_request_merge', 'pull_request_comment',
+            'change_status_issue', 'comment_issue', 'pull_request_close',
             ]:
         item = pagure.lib.model.ACL(
             name=acl,
