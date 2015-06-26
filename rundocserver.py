f8556c5a0f51b51f3c8fe6b6579cc825210bcf53
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-06-26 09:54:59 +0200

    Add a rundocserver script allowing to run the documentation server

---
+++ rundocserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+# These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
+import pkg_resources
+
+import sys
+from werkzeug.contrib.profiler import ProfilerMiddleware
+
+from pagure import APP
+from pagure.docs_server import APP
+APP.debug = True
+
+if '--profile' in sys.argv:
+    APP.config['PROFILE'] = True
+    APP.wsgi_app = ProfilerMiddleware(APP.wsgi_app, restrictions=[30])
+
+APP.run(port=5001)
