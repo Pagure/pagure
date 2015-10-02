bf8489e598de2946b4805c3927e10b905a6a9005
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-10-02 09:33:57 +0200

    Add an alembic revision file to add the closed_at field to the PR table

---
+++ alembic/versions/1cd0a853c697_add_closed_at_field_in_pr.py
@@ -0,0 +1,34 @@
+"""Add closed_at field in PR
+
+
+Revision ID: 1cd0a853c697
+Revises: 6190226bed0
+Create Date: 2015-10-02 09:32:15.370676
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1cd0a853c697'
+down_revision = '6190226bed0'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    ''' Add the column closed_at to the table pull_requests.
+    '''
+    op.add_column(
+        'pull_requests',
+        sa.Column(
+            'closed_at',
+            sa.DateTime,
+            nullable=True,
+        )
+    )
+
+
+def downgrade():
+    ''' Remove the column closed_at from the table pull_requests.
+    '''
+    op.drop_column('pull_requests', 'closed_at')
