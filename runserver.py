fb4210b4e6b40f24022b0a0db331ac5156296f15
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-10-07 09:53:18 +0200

    pep8 fix

--- runserver.py
+++ runserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-## These two lines are needed to run on EL6
+# These two lines are needed to run on EL6
 __requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
 import pkg_resources
 
