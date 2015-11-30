59554a1cd87a3fdd3f8c1d29317adbd964efd361
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-11-30 13:01:52 +0100

    Let's inform the user and change the tense of the message

--- pagure/ui/fork.py
+++ pagure/ui/fork.py
@@ -727,7 +727,7 @@ def fork_project(repo, username=None):
 
     if pagure.lib.get_project(
             SESSION, repo.name, user=flask.g.fas_user.username):
-        flask.flash('You have already forked this project', 'error')
+        flask.flash('You had already forked this project')
         return flask.redirect(flask.url_for(
             'view_repo', repo=repo.name, username=flask.g.fas_user.username
         ))
