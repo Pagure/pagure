c064eeae333d8398ae19443feb436fecbbf3ca8a
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-12-18 12:38:26 +0100

    Add a `New User` button on the page listing all the users
    Adjust the unit-tests for this change

--- tests/test_progit_flask_ui_app.py
+++ tests/test_progit_flask_ui_app.py
@@ -97,7 +97,7 @@ class PagureFlaskApptests(tests.Modeltests):
         output = self.app.get('/users/?page=abc')
         self.assertEqual(output.status_code, 200)
         self.assertIn(
-            '<h2 class="m-b-1">Users <span class="label '
+            '<h2 class="m-b-1">\n        Users <span class="label '
             'label-default">2</span></h2', output.data)
         self.assertIn(
             '<a class="project_link logo_link" href="/user/pingou">',
