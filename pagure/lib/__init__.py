0110a9f099678b4c837d62de974943f1e9577bbf
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-12-22 19:54:17 +0100

    Forward the issue id when adding a comment to an issue

--- pagure/lib/__init__.py
+++ pagure/lib/__init__.py
@@ -240,6 +240,7 @@ def add_issue_comment(session, issue, comment, user, ticketfolder,
         else:
             REDIS.publish('pagure.%s' % issue.uid, json.dumps({
                 'comment_id': issue_comment.id,
+                'issue_id': issue.id,
                 'comment_added': text2markdown(issue_comment.comment),
                 'comment_user': issue_comment.user.user,
                 'avatar_url': avatar_url(issue_comment.user.user, size=16),
