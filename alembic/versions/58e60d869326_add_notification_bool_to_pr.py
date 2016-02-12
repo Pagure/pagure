c4361a7813cb5920de679807c32fc1d6efafab94
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-02-12 15:07:07 +0100

    Add an alembic migration script adding the notification field

---
+++ alembic/versions/58e60d869326_add_notification_bool_to_pr.py
@@ -0,0 +1,33 @@
+"""add notification bool to PR
+
+Revision ID: 58e60d869326
+Revises: 1b6d7dc5600a
+Create Date: 2016-02-12 12:39:07.839530
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '58e60d869326'
+down_revision = '1b6d7dc5600a'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    ''' Add the column merge_status to the table projects.
+    '''
+    op.add_column(
+        'pull_request_comments',
+        sa.Column('notification', sa.Boolean, default=False, nullable=True)
+    )
+    op.execute('''UPDATE "pull_request_comments" SET notification=False;''')
+    op.alter_column(
+        'pull_request_comments', 'notification',
+        nullable=False, existing_nullable=True)
+
+
+def downgrade():
+    ''' Remove the column merge_status from the table projects.
+    '''
+    op.drop_column('pull_request_comments', 'notification')
