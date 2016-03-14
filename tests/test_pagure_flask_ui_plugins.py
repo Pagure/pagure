46e3b70dacaf2c8bd1790cf62f555111cf846d44
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-03-14 17:12:42 +0100

    Adjust the unit-tests for the new plugin

--- tests/test_pagure_flask_ui_plugins.py
+++ tests/test_pagure_flask_ui_plugins.py
@@ -69,8 +69,11 @@ class PagureFlaskPluginstests(tests.Modeltests):
         names = pagure.ui.plugins.get_plugin_names()
         self.assertEqual(
             sorted(names),
-            ['Block non fast-forward pushes', 'Fedmsg', 'IRC', 'Mail',
-             'pagure', 'pagure requests', 'pagure tickets'])
+            [
+                'Block non fast-forward pushes', 'Fedmsg', 'IRC', 'Mail',
+                'Read the Doc', 'pagure', 'pagure requests', 'pagure tickets'
+            ]
+        )
 
     def test_get_plugin(self):
         """ Test the get_plugin function. """
