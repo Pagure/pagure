4611feda7ccdd1bbd5d8d87e1e292826974c9686
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-04-09 15:55:02 +0200

    Publish the username of all the users who authored the commits pushed

--- pagure/hooks/files/fedmsg_hook.py
+++ pagure/hooks/files/fedmsg_hook.py
@@ -56,6 +56,14 @@ for line in sys.stdin.readlines():
     if not project:
         project = project_name
 
+    pushers = set()
+    for rev in revs:
+        email = pagure.lib.git.get_pusher_email(rev, abspath)
+        name = pagure.lib.git.get_pusher(rev, abspath)
+        pusher = search_user(pagure.SESSION, email=email) or name
+        pushers.add(pusher)
+
+
     if revs:
         revs.reverse()
         print "* Publishing information for %i commits" % len(revs)
@@ -68,6 +76,7 @@ for line in sys.stdin.readlines():
                 end_commit=revs[-1],
                 branch=refname,
                 forced=forced,
+                users=list(pushers),
                 agent=username,
                 repo=project.to_json(public=True)
                 if not isinstance(project, basestring) else project,
