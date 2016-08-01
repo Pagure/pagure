09feb2b04ab827f6658f2e9a58248957ae66d391
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-08-01 16:10:47 +0200

    Rework the API endpoint receiving the notifications from jenkins

--- pagure/api/ci/jenkins.py
+++ pagure/api/ci/jenkins.py
@@ -15,29 +15,33 @@ from sqlalchemy.exc import SQLAlchemyError
 import pagure
 import pagure.exceptions
 import pagure.lib
+import pagure.lib.lib_ci as lib_ci
 from pagure import APP, SESSION
 from pagure.api import API, APIERROR
 
 
 @API.route('/ci/jenkins/<pagure_ci_token>/build-finished', methods=['POST'])
-def ci_notification(pagure_ci_token):
+def jenkins_ci_notification(pagure_ci_token):
     """ Flag a pull-request based on the info provided by the CI service.
     """
 
+    project = lib_ci.get_project_by_ci_token(SESSION, pagure_ci_token)
+    if not project:
+        flask.abort(404, 'No project corresponding to this CI token')
+
+    data = flask.request.json()
+    if not data:
+        flask.abort(400, "Bad Request: No JSON retrived")
+
+    build_id = data.get('build', {}).get('number')
+    if not build_it:
+        flask.abort(400, "Bad Request: No build ID retrived")
+
     try:
-        data = flask.request.json()
-        cfg = jenkins_hook.get_configs(
-            data['name'], jenkins_hook.Service.JENKINS)[0]
-        build_id = data['build']['number']
+        lib_ci.process_jenkins_build(project, build_id)
+    except pagure.exceptions.PagureException as err:
+        APP.logger.error('Error processing jenkins notification', exc_info=err)
+        flask.abort(400, "Bad Request: %s" % err)
 
-        if not constant_time.bytes_eq(
-                  to_bytes(pagure_ci_token), to_bytes(cfg.pagure_ci_token)):
-            return ('Token mismatch', 401)
-
-    except (TypeError, ValueError, KeyError, jenkins_hook.ConfigNotFound) as exc:
-        APP.logger.error('Error processing jenkins notification', exc_info=exc)
-        flask.abort(400, "Bad Request")
-
-    APP.logger.info('Received jenkins notification')
-    pagure_ci.process_build(APP.logger, cfg, build_id)
+    APP.logger.info('Successfully proccessed jenkins notification')
     return ('', 204)
