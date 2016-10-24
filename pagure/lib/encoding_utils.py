cdbf51e42597605e5c087c0d394d01f51e8fd5bc
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-10-24 08:35:08 +0200

    If chardet do not return any result, go for the default one

--- pagure/lib/encoding_utils.py
+++ pagure/lib/encoding_utils.py
@@ -81,6 +81,8 @@ def detect_encodings(data):
     detector.reset()
     detector.feed(data)
     result = detector.close()
+    if not result:
+        return {'ascii': 1.0}
     encodings = {result['encoding']: result['confidence']}
     for prober in detector._mCharSetProbers:
         if prober:
