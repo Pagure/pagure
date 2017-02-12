83b88ccec9f1b1ae2986295b5a0687dc6d3923bd
Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
2017-02-12 11:18:05 +0530

    Fix docstring for get_authorized_project

--- pagure/__init__.py
+++ pagure/__init__.py
@@ -375,20 +375,21 @@ def is_repo_user(repo_obj):
 
 
 def get_authorized_project(session, project_name, user=None, namespace=None):
-    """ Retrieving the project with user permission constraint
+    ''' Retrieving the project with user permission constraint
 
-        :param session: The SQLAlchemy session to use
-        :type  session: sqlalchemy.orm.session.Session
-        :param project_name: Name of the project on pagure
-        :type  project_name: String
-        :param user: Pagure username
-        :type  user: String
-        :param namespace: Pagure namespace
-        :type  namespace: String
-        :return: The project object if project is public or user has
-                  permissions for the project else it returns None
-        :rtype:  pagure.lib.models.project
-    """
+    :arg session: The SQLAlchemy session to use
+    :type session: sqlalchemy.orm.session.Session
+    :arg project_name: Name of the project on pagure
+    :type project_name: String
+    :arg user: Pagure username
+    :type user: String
+    :arg namespace: Pagure namespace
+    :type namespace: String
+    :return: The project object if project is public or user has
+                permissions for the project else it returns None
+    :rtype: Project or [Project]
+
+    '''
     repo = pagure.lib._get_project(session, project_name, user, namespace)
 
     if repo and repo.private and not is_repo_admin(repo):
