eec03356b44d2cf9080887ce0c7e42259751ef7a
Pierre-Yves Chibon <pingou@pingoured.fr>
2017-03-04 23:27:26 +0100

    Document some existing API endpoints that were missing from the doc

--- pagure/api/__init__.py
+++ pagure/api/__init__.py
@@ -447,6 +447,10 @@ def api():
         issues.append(load_doc(issue.api_view_issue))
         issues.append(load_doc(issue.api_view_issue_comment))
         issues.append(load_doc(issue.api_comment_issue))
+        issues.append(load_doc(issue.api_update_custom_field))
+        issues.append(load_doc(issue.api_change_status_issue))
+        issues.append(load_doc(issue.api_assign_issue))
+        issues.append(load_doc(issue.api_subscribe_issue))
 
     ci_doc = []
     if pagure.APP.config.get('PAGURE_CI_SERVICES', True):
