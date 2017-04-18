55a1560d7f98ce0742f88feddead347ef973bf21
Pierre-Yves Chibon <pingou@pingoured.fr>
2017-04-18 17:53:01 +0200

    Hopefully last flake8 fix

--- pagure/__init__.py
+++ pagure/__init__.py
@@ -10,7 +10,7 @@
 
 # These two lines are needed to run on EL6
 __requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
-import pkg_resources  # noqa: E402
+import pkg_resources  # noqa: E402,F401
 
 __version__ = '2.14.2'
 __api_version__ = '0.12'
