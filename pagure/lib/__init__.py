ad88b3301b407291e7470fc4c586b2b986470648
Tim Flink <tflink@fedoraproject.org>
2017-05-26 19:02:00 +0000

    changing order for other PAGURE_CI comparisons
    Merges https://pagure.io/pagure/pull-request/2314

--- pagure/lib/__init__.py
+++ pagure/lib/__init__.py
@@ -1134,7 +1134,7 @@ def add_pull_request_comment(session, request, commit, tree_id, filename,
         # Send notification to the CI server, if the comment added was a
         # notification and the PR is still open and project is not private
         if notification and request.status == 'Open' \
-            and request.project.ci_hook and PAGURE_CI \
+            and PAGURE_CI and request.project.ci_hook\
                 and not request.project.private:
             REDIS.publish('pagure.ci', json.dumps({
                 'ci_type': request.project.ci_hook.ci_type,
@@ -1153,7 +1153,7 @@ def add_pull_request_comment(session, request, commit, tree_id, filename,
 
     if trigger_ci and comment.strip().lower() in trigger_ci:
         # Send notification to the CI server
-        if REDIS and request.project.ci_hook and PAGURE_CI:
+        if REDIS and PAGURE_CI and request.project.ci_hook:
             REDIS.publish('pagure.ci', json.dumps({
                 'ci_type': request.project.ci_hook.ci_type,
                 'pr': request.to_json(public=True, with_comments=False)
