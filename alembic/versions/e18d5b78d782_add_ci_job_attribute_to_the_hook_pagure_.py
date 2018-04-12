a2657d11e3fc4ceec8e3c8e84f6afde591f04e5f
Clement Verna <cverna@tutanota.com>
2018-04-12 15:03:10 +0200

    Migrate the jenkins job data to new db schema
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- alembic/versions/e18d5b78d782_add_ci_job_attribute_to_the_hook_pagure_.py
+++ alembic/versions/e18d5b78d782_add_ci_job_attribute_to_the_hook_pagure_.py
@@ -21,7 +21,31 @@ def upgrade():
         sa.Column('ci_job', sa.String(255), nullable=True, unique=False)
     )
 
+    con = op.get_bind()
+    results = con.execute('SELECT id, ci_url FROM hook_pagure_ci')
+
+    for id, url in results:
+        ci_job = url.split('/job/', 1)[1].split('/', 1)[0]
+        ci_url = url.split('/job/')[0]
+        op.execute(
+            "UPDATE hook_pagure_ci SET ci_job='{}' WHERE id = '{}'".format(ci_job, id))
+        op.execute(
+            "UPDATE hook_pagure_ci SET ci_url='{}' WHERE id = '{}'".format(ci_url, id))
+
+    op.alter_column(
+        'hook_pagure_ci', 'ci_job',
+        nullable=False, existing_nullable=True)
+
 
 def downgrade():
     ''' Revert the ci_job column added'''
+
+    con = op.get_bind()
+    results = con.execute('SELECT id, ci_url, ci_job FROM hook_pagure_ci')
+
+    for id, url, job in results:
+        ci_url = url + '/job/' + job + '/'
+        op.execute(
+            "UPDATE hook_pagure_ci SET ci_url='{}' WHERE id = '{}'".format(ci_url, id))
+
     op.drop_column('hook_pagure_ci', 'ci_job')
