37d153c4be7737e96e9c9fd917308677c1aff96d
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-06-09 20:18:02 +0200

    Properly remove the hook when it's being deactivated
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- pagure/ui/plugins.py
+++ pagure/ui/plugins.py
@@ -153,7 +153,7 @@ def view_plugin(repo, plugin, username=None, namespace=None, full=True):
         else:
             try:
                 plugin.remove(repo)
-                flask.g.session.add(repo)
+                flask.g.session.delete(dbobj)
                 flask.flash('Hook %s deactivated' % plugin.name)
             except FileNotFoundException as err:
                 _log.exception(err)
