9592dbda806a87446316f697f15f310ce900f99b
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-07-04 10:51:24 +0200

    Ignore the COMPLETED notifications from jenkins
    Jenkins sends three types of notifications: STARTED, COMPLETED and
    FINALIZED.
    In order to avoid duplicating flags, we want to discard the COMPLETED
    notifications and only act on STARTED and FINALIZED.
    
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- pagure/api/ci/jenkins.py
+++ pagure/api/ci/jenkins.py
@@ -72,6 +72,16 @@ def jenkins_ci_notification(
         _log.debug("Bad Request: No build ID retrieved")
         raise pagure.exceptions.APIError(400, error_code=APIERROR.EINVALIDREQ)
 
+    build_phase = data.get('build', {}).get('phase')
+    if not build_phase:
+        _log.debug("Bad Request: No build phase retrieved")
+        raise pagure.exceptions.APIError(400, error_code=APIERROR.EINVALIDREQ)
+    if build_phase not in ["STARTED", "FINALIZED"]:
+        _log.debug(
+            "Ignoring phase: %s - not in the list: STARTED, FINALIZED",
+            build_phase)
+        raise pagure.exceptions.APIError(400, error_code=APIERROR.EINVALIDREQ)
+
     try:
         lib_ci.process_jenkins_build(
             flask.g.session,
