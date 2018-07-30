9ce3f8b9f2a5fbe3268c746726f38fdf2994b29a
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-07-30 11:35:18 +0200

    Use lower or equal release of pygit2 vs libgit2
    This fixes the current situation where libgit2 released a new version: 0.26.5
    which doesn't need any changes in pygit2, so there won't be a release made
    there. So we want to use the pygit2 version just below 0.26.4 rather than
    bumping to 0.27.
    
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -33,7 +33,7 @@ fi
 
 pip install --upgrade tox
 pip install --upgrade --force-reinstall pygments chardet
-pip3 install "pygit2 == `rpm -q libgit2 --queryformat='%{version}'`"
+pip3 install "pygit2 <= `rpm -q libgit2 --queryformat='%{version}'`"
 parallel -v ::: \
 "tox --sitepackages -e 'py27-flask011-ci' -- -v --with-xcoverage --cover-erase --cover-package=pagure" \
 "tox --sitepackages -e 'py34-flask011-ci' -- -v --with-xcoverage --cover-erase --cover-package=pagure"
