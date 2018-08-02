b5a052a8ecae028d0c5e3bc1c38651a6a0485034
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-08-02 10:08:32 +0200

    Use quotes when specifying a version
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -33,7 +33,7 @@ fi
 
 # Apparently newer requests has strong feeling about idna:
 # https://github.com/requests/requests/commit/991e8b76b7a9d21f698b24fa
-pip install --upgrade tox trollius idna<2.8
+pip install --upgrade tox trollius "idna<2.8"
 pip install --upgrade --force-reinstall pygments chardet
 pip3 install "pygit2 <= `rpm -q libgit2 --queryformat='%{version}'`"
 parallel -v ::: \
