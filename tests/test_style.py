d5959ab7a5e87bf86181c84475a2c33b288da571
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-08-22 18:11:53 +0200

    Enforce running black on the source when the tests are ran with python3
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- tests/test_style.py
+++ tests/test_style.py
@@ -18,6 +18,7 @@ import subprocess
 import sys
 import unittest
 
+import six
 
 REPO_PATH = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', 'pagure'))
@@ -31,9 +32,29 @@ class TestStyle(unittest.TestCase):
         This test runs flake8 on the code, and will fail if it returns a non-zero exit code.
         """
         # We ignore E712, which disallows non-identity comparisons with True and False
+        # We ignore W503, which disallows line break before binary operator
         flake8_command = [sys.executable, '-m', 'flake8', '--ignore=E712,W503', REPO_PATH]
         proc = subprocess.Popen(flake8_command, stdout=subprocess.PIPE)
         print(proc.communicate())
+
+        self.assertEqual(proc.returncode, 0)
+
+    @unittest.skipIf(
+        not (six.PY3 and sys.version_info.minor >=6),
+        "Black is only available in python 3.6+")
+    def test_code_with_black(self):
+        """Enforce black compliance on the codebase.
+
+        This test runs black on the code, and will fail if it returns a non-zero exit code.
+        """
+        black_command = [sys.executable, '-m', 'black', '-l', '79', '--check', REPO_PATH]
+        proc = subprocess.Popen(
+            black_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        print('stdout: ')
+        print(stdout.decode('utf-8'))
+        print('stderr: ')
+        print(stderr.decode('utf-8'))
 
         self.assertEqual(proc.returncode, 0)
 
