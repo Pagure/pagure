aebcbb2c4cc5a16677d6a2cb6b99e6b85deea0fb
Patrick Uiterwijk <patrick@puiterwijk.org>
2018-10-11 11:21:30 +0200

    Don't use unicode literals for git_multimail
    It uses a literal as a type() argument, which doesn't work with unicode.
    This is also a file we forked from upstream, so we shouldn't be changing
    it up.
    This could perhaps break python3 compatibility, but we should just either
    work with upstream to fix that, or rebase on the latest upstream if they
    already have.
    
    Fixes: #3884
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- pagure/hooks/files/git_multimail_upstream.py
+++ pagure/hooks/files/git_multimail_upstream.py
@@ -47,7 +47,7 @@ See the accompanying README file for the complete documentation.
 
 """
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function
 
 import sys
 import os
