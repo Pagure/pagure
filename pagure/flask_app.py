97dcb02d2d7a9456f2c7edeb0816a8b10d031502
Pierre-Yves Chibon <pingou@pingoured.fr>
2018-11-02 12:52:51 +0100

    Black and flake8 fixes
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- pagure/flask_app.py
+++ pagure/flask_app.py
@@ -129,11 +129,11 @@ def create_app(config=None):
     app.register_blueprint(PV)
 
     # Import 3rd party blueprints
-    plugin_config = flask.config.Config('')
+    plugin_config = flask.config.Config("")
     if "PAGURE_PLUGIN" in os.environ:
         plugin_config.from_envvar("PAGURE_PLUGIN")
-    for blueprint in (plugin_config.get('PLUGINS') or []):
-        logger.info('Loading blueprint: %s', blueprint.name)
+    for blueprint in plugin_config.get("PLUGINS") or []:
+        logger.info("Loading blueprint: %s", blueprint.name)
         app.register_blueprint(blueprint)
 
     themename = pagure_config.get("THEME", "default")
