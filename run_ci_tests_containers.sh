27d0e3f17c11df35331ad6dfe7310e1f062e35da
Pierre-Yves Chibon <pingou@pingoured.fr>
2019-02-11 10:23:39 +0100

    Merge changes in the host as well
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- run_ci_tests_containers.sh
+++ run_ci_tests_containers.sh
@@ -8,6 +8,21 @@ set -e
 
 echo $BRANCH $REPO
 
+if [ -n "$REPO" -a -n "$BRANCH" ]; then
+git remote rm proposed || true
+git gc --auto
+git remote add proposed "$REPO"
+git fetch proposed
+git checkout origin/master
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git merge --no-ff "proposed/$BRANCH" -m "Merge PR"
+
+echo "Running tests for branch $BRANCH of repo $REPO"
+echo "Last commits:"
+git log -2
+fi
+
 podman build --rm -t pagure-f29-rpms-py3 \
     -f dev/containers/f29-rpms-py3 \
     dev/containers
