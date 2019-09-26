4bfc07b1219fe844f7eb6201d5e9bf671e16ed77
Fabien Boucher <fboucher@redhat.com>
2019-09-26 09:39:47 +0200

    black fix

--- tests/test_pagure_send_notification.py
+++ tests/test_pagure_send_notification.py
@@ -31,41 +31,52 @@ class PagureHooksDefault(tests.SimplePagureTest):
         super(PagureHooksDefault, self).setUp()
         tests.create_projects(self.session)
         self.projects = tests.create_projects_git(
-            os.path.join(self.path, "repos"), bare=True)
+            os.path.join(self.path, "repos"), bare=True
+        )
         self.folder = os.path.join(self.path, "repos", "test.git")
 
     def init_test_repo(self):
         tests.add_content_git_repo(self.projects[0])
         repo = pygit2.Repository(self.projects[0])
         sha = repo.references["refs/heads/master"].peel().hex
-        project = pagure.lib.query.get_authorized_project(
-            self.session, "test")
+        project = pagure.lib.query.get_authorized_project(self.session, "test")
         return project, sha
 
     @mock.patch("pagure.hooks.default.send_fedmsg_notifications")
-    def test_send_action_notification(
-            self, fedmsg):
+    def test_send_action_notification(self, fedmsg):
         project, sha = self.init_test_repo()
         pagure.hooks.default.send_action_notification(
-            self.session, "tag", "bar",
-            project, self.folder, "pingou", "master", sha)
+            self.session,
+            "tag",
+            "bar",
+            project,
+            self.folder,
+            "pingou",
+            "master",
+            sha,
+        )
         (_, args, kwargs) = fedmsg.mock_calls[0]
-        self.assertEqual(args[1], 'git.tag.bar')
-        self.assertEqual(args[2]['repo']['name'], 'test')
-        self.assertEqual(args[2]['rev'], sha)
+        self.assertEqual(args[1], "git.tag.bar")
+        self.assertEqual(args[2]["repo"]["name"], "test")
+        self.assertEqual(args[2]["rev"], sha)
 
     @mock.patch("pagure.hooks.default.send_fedmsg_notifications")
-    def test_send_notifications(
-            self, fedmsg):
+    def test_send_notifications(self, fedmsg):
         project, sha = self.init_test_repo()
         pagure.hooks.default.send_notifications(
-            self.session, project, self.folder,
-            "pingou", "master", [sha], False)
+            self.session,
+            project,
+            self.folder,
+            "pingou",
+            "master",
+            [sha],
+            False,
+        )
         (_, args, kwargs) = fedmsg.mock_calls[0]
-        self.assertEqual(args[1], 'git.receive')
-        self.assertEqual(args[2]['repo']['name'], 'test')
-        self.assertEqual(args[2]['start_commit'], sha)
-        self.assertEqual(args[2]['forced'], False)
+        self.assertEqual(args[1], "git.receive")
+        self.assertEqual(args[2]["repo"]["name"], "test")
+        self.assertEqual(args[2]["start_commit"], sha)
+        self.assertEqual(args[2]["forced"], False)
 
 
 if __name__ == "__main__":
