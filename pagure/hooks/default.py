Pagure/pagure

commit 07b517e2fa1f40ea872cf60eb06b92ce2cd88e09

Get the default branch of the target repo when linking for new PR
Rather than assuming the default branch is the same in the source
and the target repo, we generate the new PR url using the default
branch of the target repo.

Fixes https://pagure.io/pagure/issue/5086

Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

pagure/hooks/default.py
@@ -232,15 +232,17 @@ def send_notifications(
             )
 
 
-def inform_pull_request_urls(
-    session, project, commits, refname, default_branch, username
-):
+def inform_pull_request_urls(session, project, commits, refname, username):
     """Inform the user about the URLs to open a new pull-request or visit
     the existing one.
     """
     target_repo = project
     if project.is_fork:
         target_repo = project.parent
+
+    taget_repo_obj = pygit2.Repository(target_repo.repopath("main"))
+    if not taget_repo_obj.is_empty and not taget_repo_obj.head_is_unborn:
+        default_branch = taget_repo_obj.head.shorthand
 
     pr_uids = []
 

commit 8c980d3eee4d8642b41c494b7e55fe44fd60b85b

Fix inform_pull_request_urls in the default hook
The function requires 5 arguments, not 6.
Broken in 07b517e2fa1f40ea872cf60eb06b92ce2cd88e09

Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

pagure/hooks/default.py
@@ -446,7 +446,6 @@ class DefaultRunner(BaseRunner):
                     project,
                     commits,
                     refname,
-                    default_branch,
                     username,
                 )
             )