999b6616cfc6e57f74a1a1cb8a0bc8210d50ae9a
Anatoli Babenia <anatoli@rainforce.org>
2021-06-14 14:54:19 +0300

    Run Pagure in Docker from any dir

--- dev/docker-start.sh
+++ dev/docker-start.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-[[ -f dev/docker-compose.yml ]] || (echo "please run this command from the root of the pagure git repo" && exit 1)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIR="$( dirname "$SCRIPT_DIR" )"
 
+cd "$ROOT_DIR"
+
+echo "Pagure Env: $ROOT_DIR/lcl"
 mkdir -p lcl/{repos,remotes,attachments,releases}
 
 docker image build -f dev/containers/base -t pagure-base .
