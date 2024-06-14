a594433199e14bc5526731f798542e5517a08f8f
Dominik Wombacher <dominik@wombacher.cc>
2024-06-14 21:14:59 +0200

    feat(createdb): Honor env vars, validate parameters.
    Use values of env vars PAGURE_CONFIG and ALEMBIC_CONFIG if set. Get full path to alembic config and perform check if file exist, similar as for pagure config.

--- createdb.py
+++ createdb.py
@@ -33,17 +33,23 @@ if args.config:
     if not config.startswith("/"):
         here = os.path.join(os.path.dirname(os.path.abspath(__file__)))
         config = os.path.join(here, config)
+    if not os.path.exists(config):
+        print("The file `{0}` could not be found".format(config))
+        sys.exit(2)
     os.environ["PAGURE_CONFIG"] = config
 
-
 if args.alembic_cfg:
-    if not args.alembic_cfg.endswith("alembic.ini"):
+    alembic_config = args.alembic_cfg
+    if not alembic_config.startswith("/"):
+        here = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+        alembic_config = os.path.join(here, alembic_config)
+    if not alembic_config.endswith("alembic.ini"):
         print("--initial should point to the alembic.ini file to use.")
         sys.exit(1)
-    if not os.path.exists(args.alembic_cfg):
-        print("The file `{0}` could not be found".format(args.alembic_cfg))
+    if not os.path.exists(alembic_config):
+        print("The file `{0}` could not be found".format(alembic_config))
         sys.exit(2)
-
+    os.environ["ALEMBIC_CONFIG"] = alembic_config
 
 import pagure.config
 from pagure.lib import model
@@ -52,7 +58,7 @@ _config = pagure.config.reload_config()
 
 model.create_tables(
     _config["DB_URL"],
-    _config.get("PATH_ALEMBIC_INI", args.alembic_cfg),
+    _config.get("PATH_ALEMBIC_INI", os.environ["ALEMBIC_CONFIG"]),
     acls=_config.get("ACLS", {}),
     debug=True,
 )
